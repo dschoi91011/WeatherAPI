93259784c10a780fa37527b4b7c514172148c401
unknown <daniel.choi91011@gmail.com>
2024-09-09 14:26:41 -0700

    querySelectors defined

---
+++ script.js
@@ -0,0 +1,6 @@
+const searchInput = document.querySelector('.search-input');
+const locationButton = document.querySelector('.location-button');
+const currentWeatherDiv = document.querySelector('.current-weather');
+const hourlyWeather = document.querySelector('.hourly-weather .weather-list');
+
+const API_KEY = 'YOUR-API-KEY-HERE' // <---------------- API KEY
